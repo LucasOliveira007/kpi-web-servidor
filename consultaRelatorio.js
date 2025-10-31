05e99f32ea7a6f3771152ae969d06389aa0e01f1
Lucas Oliveira <lukas-oliveira72@hotmail.com>
2025-10-30 23:20:31 -0300

    Delete consultaRelatorio.js

--- consultaRelatorio.js
+++
@@ -1,6 +0,0 @@
-// Função para gerar o relatório
-function gerarRelatorio() {
-    const conteudo = "<h1>Relatório Gerado</h1><p>Parabéns, você está logado!</p>";
-    document.getElementById('conteudoRelatorio').innerHTML = conteudo;
-}
-
