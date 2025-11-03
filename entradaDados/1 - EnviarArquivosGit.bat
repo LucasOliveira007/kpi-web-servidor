e64d64dee16cb03ded24138808b60faeccbd7be8
Lucas Oliveira <lukas-oliveira72@hotmail.com>
2025-11-03 13:46:56 -0300

    Atualiza├º├úo da pasta entradaDados via .bat

--- entradaDados/1 - EnviarArquivosGit.bat
+++ entradaDados/1 - EnviarArquivosGit.bat
@@ -3,8 +3,9 @@ echo 🔄 Enviando pasta entradaDados para o GitHub...
 
 cd /d "C:\Users\GMF\3D Objects\Kpi_Web_Atualizador"
 
+git pull origin main --allow-unrelated-histories
 git add entradaDados
-git commit -m "Atualizado via .bat"
+git commit -m "Atualização da pasta entradaDados via .bat"
 git push origin main
 
 echo ✅ Pasta entradaDados enviada com sucesso!
