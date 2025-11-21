d50db4a0058cd02f941eb9336d34cd8469b05496
Suelen <Suelen@GMF.local>
2025-11-21 12:04:05 -0300

    AtualizaþÒo da pasta entradaDados via .bat (Suelen)

---
+++ entradaDados/2 - AtualizarServidor.bat
@@ -0,0 +1,7 @@
+@echo off
+echo 🔄 Forçando atualização no Render...
+
+curl -X GET "https://api.render.com/deploy/srv-d421np3ipnbc73bva950?key=2VF3etWgk40"
+
+echo ✅ Deploy disparado com sucesso!
+pause
